2a7ba23529f0a41075a2661c02807bedc57b85ea
CarlosAttanasio <attanasiocarlos@gmail.com>
2021-05-11 19:08:46 -0300

    Agregado comentarios a Parcela

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -1,12 +1,24 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
+  /**
+  * Redundancia minima/Simplicidad: No es necesario un contador de plantas ya que se puede resolver utilizando el tamaño de la
+  * lista de plantas.
+  */
+  /**
+  * Mutabilidad controlada: Se está exponiendo la lista de plantas y se está accediendo directamente desde otra clase.
+  */
   val plantas = mutableListOf<Planta>()
   var cantidadPlantas = 0
 
   fun superficie() = ancho * largo
   fun cantidadMaximaPlantas() =
     if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo
+
+  /**
+  * Robustez: Se pide que el metodo arroje un error, no un comentario. Puede causar inconsistencias.
+  */
+
 
   fun plantar(planta: Planta) {
     if (cantidadPlantas == this.cantidadMaximaPlantas()) {
@@ -37,7 +49,10 @@ class Agricultora(val parcelas: MutableList<Parcela>) {
         planta -> planta.daSemillas()
       }
     }
-
+  /**
+  * Abstraccion-Rehusabilidad: Debería utilizar el metodo plantar que ya existe en la clase parcela y no acceder
+  * directamente a la lista.
+  */
   fun plantarEstrategicamente(planta: Planta) {
     val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!
     laElegida.plantas.add(planta)
